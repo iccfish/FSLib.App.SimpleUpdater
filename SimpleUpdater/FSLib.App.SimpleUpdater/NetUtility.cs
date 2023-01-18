12ec21fa1c513df5a69ab5f30f002dd7d09b0ae6
木魚(iFish) <iccfish@qq.com>
2023-01-18 18:03:26 +0800

    修正兼容性

--- SimpleUpdater/FSLib.App.SimpleUpdater/NetUtility.cs
+++ SimpleUpdater/FSLib.App.SimpleUpdater/NetUtility.cs
@@ -1,18 +1,22 @@
 ﻿namespace FSLib.App.SimpleUpdater
 {
-	using System.Net;
+    using System;
+    using System.Net;
 
-	internal class NetUtility
-	{
-		public static void EnableCompatibility()
-		{
-#if NET20 || NET35 || NET40
-			ServicePointManager.SecurityProtocol =
-				SecurityProtocolType.Ssl3
-				| SecurityProtocolType.Tls
-				| (SecurityProtocolType)3072 //TLS 1.2
-				| (SecurityProtocolType)768; //TLS 1.1;
+    internal class NetUtility
+    {
+        public static void EnableCompatibility()
+        {
+            if (Environment.OSVersion.Version.Major > 5)
+            {
+#if NET20 || NET35 || NET40 || NET45
+                ServicePointManager.SecurityProtocol =
+                    SecurityProtocolType.Ssl3
+                    | SecurityProtocolType.Tls
+                    | (SecurityProtocolType)3072 //TLS 1.2
+                    | (SecurityProtocolType)768; //TLS 1.1;
 #endif
-		}
-	}
+            }
+        }
+    }
 }
